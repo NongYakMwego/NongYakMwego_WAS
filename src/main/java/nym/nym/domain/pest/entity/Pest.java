62b2e8a074a64463ed07c4787f4ff69828e7d225
tkv00 <tkv0098@gmail.com>
2025-05-18 07:20:03 +0900

    chore : Pest Entity 생성

--- src/main/java/nym/nym/domain/pest/entity/Pest.java
+++ src/main/java/nym/nym/domain/pest/entity/Pest.java
@@ -1,4 +1,19 @@
 package nym.nym.domain.pest.entity;
 
-public class Pest {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import nym.nym.global.common.entity.BaseEntity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pest_id",nullable = false,unique = true)
+    private Long pestId;
+
+    @Column(name = "pest_name",nullable = false,length = 50)
+    private String pestName;
 }
