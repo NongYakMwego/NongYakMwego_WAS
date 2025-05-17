eb3707669aec5975fc3a725c1da61e83b73f74a6
tkv00 <tkv0098@gmail.com>
2025-05-18 07:45:09 +0900

    refact : Estate Entity 기본키 생성 전략 UUID 변경

--- src/main/java/nym/nym/domain/estate/entity/Estate.java
+++ src/main/java/nym/nym/domain/estate/entity/Estate.java
@@ -1,20 +1,24 @@
 package nym.nym.domain.estate.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import nym.nym.global.common.entity.BaseEntity;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Estate extends BaseEntity {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
     @Column(name = "estate_id",nullable = false,unique = true)
-    private String estateId;
+    private UUID estateId;
 
     @Embedded
     private Location location;
