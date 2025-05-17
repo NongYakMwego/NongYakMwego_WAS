5bd32a0a923f44cf0959c18b9d9f8368ce54d1a2
tkv00 <tkv0098@gmail.com>
2025-05-18 04:55:57 +0900

    chore : 프로젝트 초기화

---
+++ src/main/java/nym/nym/NymApplication.java
@@ -0,0 +1,13 @@
+package nym.nym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nym/nym/NymApplicationTests.java
@@ -0,0 +1,13 @@
+package nym.nym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
