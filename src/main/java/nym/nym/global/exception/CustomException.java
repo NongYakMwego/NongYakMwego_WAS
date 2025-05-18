87ed03de7071ebc4c650e60f71c4f874cc63541b
tkv00 <tkv0098@gmail.com>
2025-05-18 20:39:53 +0900

    feat : GlobalExceptionHandler 구현

--- src/main/java/nym/nym/global/exception/CustomException.java
+++ src/main/java/nym/nym/global/exception/CustomException.java
@@ -2,9 +2,14 @@ package nym.nym.global.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import nym.nym.global.common.type.ErrorCode;
 
 @Getter
 @RequiredArgsConstructor
 public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
 
+    public String getMessage(){
+        return errorCode.getMessage();
+    }
 }
